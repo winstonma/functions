4943baa9601fa5f2a613f24d01e8b86a039c212d
unknown <winstonma@ymail.com>
2020-02-05 20:29:32 +0800

    Test the Youtube API

---
+++ src/lambda/youtube_full.js
@@ -0,0 +1,30 @@
+import ytdl from "ytdl-core";
+
+exports.handler = async (event, context) => {
+  let response;
+
+  const videoID =
+    event.queryStringParameters && event.queryStringParameters.videoID;
+
+  try {
+    response = await ytdl.getInfo(videoID);
+    // handle response
+      const format = ytdl.chooseFormat(response.formats, { quality: "136" })
+      response.formats = format
+      delete response["related_videos"]
+      delete response["player_response"]
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
+    };
+  }
+
+  return {
+    statusCode: 200,
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(response)
+  };
+};
