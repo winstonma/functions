03c0a9f196a652c2b77f3d875112a4efc2a87219
unknown <winstonma@ymail.com>
2020-02-12 11:22:09 +0800

    Just rewrite the code for readability

--- src/lambda/youtube_full.js
+++ src/lambda/youtube_full.js
@@ -6,28 +6,27 @@ exports.handler = async (event, context) => {
   const videoID =
     event.queryStringParameters && event.queryStringParameters.videoID;
 
-  try {
-    response = await ytdl.getInfo(videoID);
-    // handle response
-    const format = ytdl.chooseFormat(response.formats, { quality: "136" });
-    response.formats = format;
-    delete response["related_videos"];
-    delete response["player_response"];
-  } catch (err) {
-    return {
-      statusCode: err.statusCode || 500,
+  return ytdl
+    .getInfo(videoID)
+    .then(response => {
+      const format = ytdl.chooseFormat(response.formats, { quality: "136" });
+      response.formats = format;
+      delete response["related_videos"];
+      delete response["player_response"];
+      return response;
+    })
+    .then(response => ({
+      statusCode: 200,
+      headers: {
+        "content-type": "application/json",
+        "cache-control": "no-cache no-store max-age=0 must-revalidate"
+      },
+      body: JSON.stringify(response)
+    }))
+    .catch(error => ({
+      statusCode: error.statusCode || 500,
       body: JSON.stringify({
-        error: err.message
+        error: error.message
       })
-    };
-  }
-
-  return {
-    statusCode: 200,
-    headers: {
-      "content-type": "application/json",
-      "cache-control": "no-cache no-store max-age=0 must-revalidate"
-    },
-    body: JSON.stringify(response)
-  };
+    }));
 };
