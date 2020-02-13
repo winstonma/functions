0881a27bbb21c7056dd300c4e33b0e656c9eac7d
unknown <winstonma@ymail.com>
2020-02-13 15:37:01 +0800

    Make the code looks cleaner

--- src/lambda/youtube.js
+++ src/lambda/youtube.js
@@ -6,30 +6,29 @@ exports.handler = async (event, context) => {
   const videoID =
     event.queryStringParameters && event.queryStringParameters.videoID;
 
-  return (
-    ytdl
-      .getInfo(videoID)
-      .then(response => {
-        /*
-        const format = ytdl.chooseFormat(response.formats, { quality: "136" });
-        response.formats = format;
-        delete response["related_videos"];
-        delete response["player_response"];
-        */
-        return {
-          statusCode: 200,
-          headers: {
-            "content-type": "application/json",
-            "cache-control": "no-cache no-store max-age=0 must-revalidate"
-          },
-          body: JSON.stringify(response)
-        };
+  return ytdl
+    .getInfo(videoID)
+    /*
+    .then(response => {
+      const format = ytdl.chooseFormat(response.formats, { quality: "136" });
+      response.formats = format;
+      delete response["related_videos"];
+      delete response["player_response"];
+      return response;
+    })
+    */
+    .then(data => ({
+      statusCode: 200,
+      headers: {
+        "content-type": "application/json",
+        "cache-control": "no-cache no-store max-age=0 must-revalidate"
+      },
+      body: JSON.stringify(data)
+    }))
+    .catch(error => ({
+      statusCode: error.statusCode || 500,
+      body: JSON.stringify({
+        error: error.message
       })
-      .catch(error => ({
-        statusCode: error.statusCode || 500,
-        body: JSON.stringify({
-          error: error.message
-        })
-      }))
-  );
+    }));
 };
--- src/lambda/youtube_full.js
+++ src/lambda/youtube_full.js
@@ -13,15 +13,16 @@ exports.handler = async (event, context) => {
       response.formats = format;
       delete response["related_videos"];
       delete response["player_response"];
-      return {
-        statusCode: 200,
-        headers: {
-          "content-type": "application/json",
-          "cache-control": "no-cache no-store max-age=0 must-revalidate"
-        },
-        body: JSON.stringify(response)
-      };
+      return response;
     })
+    .then(data => ({
+      statusCode: 200,
+      headers: {
+        "content-type": "application/json",
+        "cache-control": "no-cache no-store max-age=0 must-revalidate"
+      },
+      body: JSON.stringify(data)
+    }))
     .catch(error => ({
       statusCode: error.statusCode || 500,
       body: JSON.stringify({
